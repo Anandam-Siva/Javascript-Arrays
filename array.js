c63a815ad65d0729c22f0fe8dcb1d79de0910a73
Anandam Siva <sivadevops80123@gmail.com>
2022-08-16 15:29:10 +0530

    Javascript arrays Description

---
+++ array.js
@@ -0,0 +1,12 @@
+
+let friends = ["siva", "chinnu", "aaryan"];
+// --------------0--------1----------2----
+console.log(friends);
+let year = new Array(1991, 13320, 123432);
+console.log(city);
+// arrays are 0 index baed
+console.log(friends[1]);
+console.log(city.length);
+console.log(friends[friends.length - 1]);
+
+// console.log(city.push(1992));
