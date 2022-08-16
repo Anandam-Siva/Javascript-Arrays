4f422027988864e03f659bd4a72eac6ef4591a61
Anandam Siva <sivadevops80123@gmail.com>
2022-08-16 15:32:38 +0530

    Fixed Issue

--- array.js
+++ array.js
@@ -2,7 +2,7 @@
 let friends = ["siva", "chinnu", "aaryan"];
 // --------------0--------1----------2----
 console.log(friends);
-let year = new Array(1991, 13320, 123432);
+let city = new Array(1991, 13320, 123432);
 console.log(city);
 // arrays are 0 index baed
 console.log(friends[1]);
